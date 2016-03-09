828057f416c4ef6a5b013c80db1d991c42c6b936
PasternakAS <PasternakAS@6f97f952-99cc-4844-926e-59bc75dff8f2>
2016-03-09 02:07:27 +0000

    git-svn-id: svn:///U:/Repos/uloader@174 6f97f952-99cc-4844-926e-59bc75dff8f2

---
+++ branches/KTSTUsql_D_001/SrcMSTASUTPIdT5tg6sql/SrcMSTASUTPIdT5tg6sql.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO; //Path
+using System.Data; //DataTable
+using System.Data.Common; //DbConnection
+using System.Threading;
+
+using HClassLibrary;
+using uLoaderCommon;
+
+namespace SrcMSTASUTPIDT5tg6sql
+{
+    public class SrcMSTASUTPIDT5tg6sql : HHandlerDbULoaderMSTIDsql
+    {
+        public SrcMSTASUTPIDT5tg6sql(IPlugIn plugIn)
+            : base(plugIn, MODE_CURINTERVAL.CAUSE_NOT, MODE_CURINTERVAL.FULL_PERIOD)
+        {
+        }
+
+        public SrcMSTASUTPIDT5tg6sql()
+            : base(MODE_CURINTERVAL.CAUSE_NOT, MODE_CURINTERVAL.FULL_PERIOD)
+        {
+        }
+
+        protected override HHandlerDbULoader.GroupSignals createGroupSignals(int id, object[] objs)
+        {
+            return new GroupSignalsMSTASUTPT5tg6IDsql(this, id, objs);
+        }
+
+        protected override void parseValues(System.Data.DataTable table)
+        {
+            //base.parseValues (table);
+
+            DataTable tblRes = new DataTable();
+            DataRow[] rowsSgnl = null;
+            DateTime dtValue;
+            double dblValue = -1F;
+
+            tblRes.Columns.AddRange(new DataColumn[] {
+                new DataColumn (@"ID", typeof (int))
+                , new DataColumn (@"DATETIME", typeof (DateTime))
+                , new DataColumn (@"VALUE", typeof (float))
+            });
+
+            foreach (GroupSignalsMSTIDsql.SIGNALIdsql sgnl in m_dictGroupSignals[IdGroupSignalsCurrent].Signals)
+            {
+                rowsSgnl = table.Select(@"ID=" + sgnl.m_iIdLocal);
+
+                //??? если строк > 1
+                if (rowsSgnl.Length > 0)
+                {
+                    dtValue = (DateTime)rowsSgnl[0][@"DATETIME"];
+
+                    dblValue = (double)rowsSgnl[0][@"VALUE"];
+
+                    //// при необходимости найти среднее
+                    //if (sgnl.m_bAVG == true)
+                    //    dblSumValue /= 60; //cntRec
+                    //else
+                    //    ;
+                    // вставить строку
+                    tblRes.Rows.Add(new object[] {
+                        sgnl.m_idMain
+                        , dtValue
+                        , dblValue
+                    });
+                }
+                else
+                    ; // не полные данные
+            }
+
+            base.parseValues(tblRes);
+        }
+
+        private class GroupSignalsMSTASUTPT5tg6IDsql : GroupSignalsMSTIDsql
+        {
+            public GroupSignalsMSTASUTPT5tg6IDsql(HHandlerDbULoader parent, int id, object[] pars)
+                : base(parent, id, pars)
+            {
+            }
+
+            protected override void setQuery()
+            {
+                m_strQuery = string.Empty;
+                string strIds = string.Empty;
+
+                foreach (GroupSignalsSrc.SIGNALIdsql sgnl in m_arSignals)
+                    strIds += sgnl.m_iIdLocal + @",";
+                // удалить "лишнюю" запятую
+                strIds = strIds.Substring(0, strIds.Length - 1);
+
+                m_strQuery = @"SELECT [ID]"
+                    + @", [VALUE]"
+                    + @", DATEADD(HOUR, 0, [dtEnd]) as [DATETIME]"
+                    + @" FROM [dbo].[tep_allcalcs_oper]"
+                        + @" WHERE"
+                        + @" [dtEnd] >='" + DateTimeBeginFormat + @"'"
+                        + @" AND [dtEnd] <'" + DateTimeEndFormat + @"'"
+                            + @" AND [ID] IN (" + strIds + @")"
+                    ;
+            }
+        }
+    }
+
+    public class PlugIn : PlugInULoader
+    {
+        public PlugIn()
+            : base()
+        {
+            _Id = 1008;
+
+            registerType(_Id, typeof(SrcMSTASUTPIDT5tg6sql));
+        }
+
+        public override void OnEvtDataRecievedHost(object obj)
+        {
+            base.OnEvtDataRecievedHost(obj);
+        }
+    }
+}
---
+++ branches/KTSTUsql_D_001/SrcMSTKKSNAMEtoris/DbTorISSources.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+
+using HClassLibrary;
+
+namespace SrcMSTKKSNAMEtoris
+{
+    /// <summary>
+    /// Класс для описания объекта управления установленными соединенями
+    /// </summary>
+    public class DbTorISSources : DbSources
+    {
+        /// <summary>
+        /// Уникальный идентификатор Модес-Центра
+        /// </summary>
+        private const int TORIS_ID = -40815;
+        /// <summary>
+        /// Конструктор - основной (без параметров)
+        /// </summary>
+        protected DbTorISSources()
+            : base()
+        {
+        }
+        /// <summary>
+        /// Функция доступа к объекту управления установленными соединенями
+        /// </summary>
+        /// <returns>Объект управления установленными соединенями</returns>
+        public static new DbTorISSources Sources()
+        {
+            if (m_this == null)
+                m_this = new DbTorISSources();
+            else
+                ;
+
+            return (DbTorISSources)m_this;
+        }
+        /// <summary>
+        /// Регистриует клиента соединения, активным или нет, при необходимости принудительно отдельный экземпляр
+        /// </summary>
+        /// <param name="connSett">параметры соединения</param>
+        /// <param name="active">признак активности</param>
+        /// <param name="bReq">признак принудительного создания отдельного экземпляра</param>
+        /// <returns></returns>
+        public override int Register(object connSett, bool active, string desc, bool bReq = false)
+        {
+            int id = -1,
+                err = -1;
+
+            if (connSett is ConnectionSettings == true)
+                return base.Register (connSett, active, desc, bReq);
+            else
+                if (connSett is string == true) {
+                    if (m_dictDbInterfaces.ContainsKey (TORIS_ID) == true) id = m_dictDbInterfaces[TORIS_ID].ListenerRegister(); else ;
+                }
+                else
+                    ;
+
+            if (id < 0)
+            {
+                string dbNameType = string.Empty;
+                DbTSQLInterface.DB_TSQL_INTERFACE_TYPE dbType = DbTSQLInterface.DB_TSQL_INTERFACE_TYPE.ModesCentre;
+                dbNameType = dbType.ToString();
+
+                m_dictDbInterfaces.Add(TORIS_ID, new DbTorISSources((string)connSett));
+                
+                if (active == true) m_dictDbInterfaces[TORIS_ID].Start(); else ;
+                m_dictDbInterfaces[TORIS_ID].SetConnectionSettings(connSett, active);
+
+                id = m_dictDbInterfaces[TORIS_ID].ListenerRegister();
+            }
+            else
+                ;
+
+            return registerListener(TORIS_ID, id, active, out err);
+        }
+    }
+}
