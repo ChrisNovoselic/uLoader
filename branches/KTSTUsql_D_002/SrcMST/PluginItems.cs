f08bdf248619e9bf0de66a29d48f42136ba29a5c
Pasternak_AS <Pasternak_AS@6f97f952-99cc-4844-926e-59bc75dff8f2>
2016-04-28 06:13:42 +0000

    git-svn-id: svn:///U:/Repos/uloader@255 6f97f952-99cc-4844-926e-59bc75dff8f2

---
+++ branches/KTSTUsql_D_002/SrcMST/PluginItems.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO; //Path
+using System.Data; //DataTable
+using System.Data.Common; //DbConnection
+using System.Threading;
+
+using HClassLibrary;
+using uLoaderCommon;
+
+namespace SrcMST
+{
+    public class PlugIn : PlugInULoader
+    {
+        public PlugIn()
+            : base()
+        {
+            _Id = 1003;
+
+            registerType(10303, typeof(SrcMSTASUTPIDT5tg1sql));
+            registerType(10304, typeof(SrcMSTKKSNAMEsql));
+            registerType(10307, typeof(SrcMSTKKSNAMEtoris));
+            registerType(10308, typeof(SrcMSTASUTPIDT5tg6sql));
+        }
+
+        public override void OnEvtDataRecievedHost(object obj)
+        {
+            base.OnEvtDataRecievedHost(obj);
+        }
+    }
+}
