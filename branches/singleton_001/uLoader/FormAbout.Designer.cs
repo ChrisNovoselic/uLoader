bf20e9ef732215ebce443ec2daea5a3df3676bf0
Pasternak_AS <Pasternak_AS@6f97f952-99cc-4844-926e-59bc75dff8f2>
2016-06-21 02:28:45 +0000

    git-svn-id: svn:///U:/Repos/uloader@300 6f97f952-99cc-4844-926e-59bc75dff8f2

---
+++ branches/singleton_001/uLoader/FormAbout.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace uLoader
+{
+    partial class FormAbout
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAbout));
+            this.m_btnOk = new System.Windows.Forms.Button();
+            this.m_pictureBoxIconMain = new System.Windows.Forms.PictureBox();
+            this.m_labelProductVersion = new System.Windows.Forms.Label();
+            this.m_labelZ = new System.Windows.Forms.Label();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.m_linkLblAuthor = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.m_pictureBoxIconMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // m_btnOk
+            // 
+            this.m_btnOk.Location = new System.Drawing.Point(144, 103);
+            this.m_btnOk.Name = "m_btnOk";
+            this.m_btnOk.Size = new System.Drawing.Size(75, 23);
+            this.m_btnOk.TabIndex = 0;
+            this.m_btnOk.Text = "Ок";
+            this.m_btnOk.UseVisualStyleBackColor = true;
+            this.m_btnOk.Click += new System.EventHandler(this.m_btnOk_Click);
+            // 
+            // m_pictureBoxIconMain
+            // 
+            this.m_pictureBoxIconMain.Location = new System.Drawing.Point(6, 6);
+            this.m_pictureBoxIconMain.Name = "m_pictureBoxIconMain";
+            this.m_pictureBoxIconMain.Size = new System.Drawing.Size(64, 64);
+            this.m_pictureBoxIconMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.m_pictureBoxIconMain.TabIndex = 1;
+            this.m_pictureBoxIconMain.TabStop = false;
+            // 
+            // m_labelProductVersion
+            // 
+            this.m_labelProductVersion.AutoSize = true;
+            this.m_labelProductVersion.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.m_labelProductVersion.Location = new System.Drawing.Point(79, 9);
+            this.m_labelProductVersion.Name = "m_labelProductVersion";
+            this.m_labelProductVersion.Size = new System.Drawing.Size(105, 13);
+            this.m_labelProductVersion.TabIndex = 2;
+            this.m_labelProductVersion.Text = "ProductVersion...";
+            // 
+            // m_labelZ
+            // 
+            this.m_labelZ.AutoSize = true;
+            this.m_labelZ.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.m_labelZ.Location = new System.Drawing.Point(78, 31);
+            this.m_labelZ.Name = "m_labelZ";
+            this.m_labelZ.Size = new System.Drawing.Size(167, 13);
+            this.m_labelZ.TabIndex = 3;
+            this.m_labelZ.Text = "Заказчик: \"Генрация\" (НТЭЦ-5)";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(232, 84);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(131, 13);
+            this.linkLabel2.TabIndex = 15;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Pasternak_AS@sibeco.su";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(80, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Разработчик: Пастернак А.С.";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(232, 68);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(127, 13);
+            this.linkLabel1.TabIndex = 13;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Apelgans_AV@sibeco.su";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(80, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Разработчик: Апельганс А.В.";
+            // 
+            // m_linkLblAuthor
+            // 
+            this.m_linkLblAuthor.AutoSize = true;
+            this.m_linkLblAuthor.Location = new System.Drawing.Point(232, 66);
+            this.m_linkLblAuthor.Name = "m_linkLblAuthor";
+            this.m_linkLblAuthor.Size = new System.Drawing.Size(122, 13);
+            this.m_linkLblAuthor.TabIndex = 11;
+            this.m_linkLblAuthor.TabStop = true;
+            this.m_linkLblAuthor.Text = "Chrjapin_AN@sibeco.su";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Разработчик: Хряпин А.Н.";
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Location = new System.Drawing.Point(232, 53);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(122, 13);
+            this.linkLabel3.TabIndex = 16;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "Chrjapin_AN@sibeco.su";
+            // 
+            // FormAbout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 132);
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.m_linkLblAuthor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.m_labelZ);
+            this.Controls.Add(this.m_labelProductVersion);
+            this.Controls.Add(this.m_pictureBoxIconMain);
+            this.Controls.Add(this.m_btnOk);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormAbout";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "О программе...";
+            this.Load += new System.EventHandler(this.FormAbout_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.m_pictureBoxIconMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button m_btnOk;
+        private System.Windows.Forms.PictureBox m_pictureBoxIconMain;
+        private System.Windows.Forms.Label m_labelProductVersion;
+        private System.Windows.Forms.Label m_labelZ;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel m_linkLblAuthor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+    }
+}
