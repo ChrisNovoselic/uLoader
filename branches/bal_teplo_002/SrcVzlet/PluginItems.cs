b774f16a7b2795c185767bc85b8d96551b0693f5
Apelgans_AV <Apelgans_AV@6f97f952-99cc-4844-926e-59bc75dff8f2>
2016-06-23 08:05:49 +0000

    git-svn-id: svn:///U:/Repos/uloader@302 6f97f952-99cc-4844-926e-59bc75dff8f2

---
+++ branches/bal_teplo_002/SrcVzlet/PluginItems.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO; //Path
+using System.Data; //DataTable
+using System.Data.Common; //DbConnection
+using System.Threading;
+
+using HClassLibrary;
+using uLoaderCommon;
+
+namespace SrcVzlet
+{
+    public class PlugIn : PlugInULoader
+    {
+        public PlugIn()
+            : base()
+        {
+            _Id = 101;
+
+            registerType(10101, typeof(SrcVzletSql));
+        }
+
+        public override void OnEvtDataRecievedHost(object obj)
+        {
+            base.OnEvtDataRecievedHost(obj);
+        }
+    }
+}
