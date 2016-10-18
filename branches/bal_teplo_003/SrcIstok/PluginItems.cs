afcaec6be742b2484668a454e8195cfddb143ac1
Apelgans_AV <Apelgans_AV@6f97f952-99cc-4844-926e-59bc75dff8f2>
2016-10-18 09:10:25 +0000

    git-svn-id: svn:///U:/Repos/uloader@381 6f97f952-99cc-4844-926e-59bc75dff8f2

---
+++ branches/bal_teplo_003/SrcIstok/PluginItems.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Globalization;
+
+using HClassLibrary;
+using uLoaderCommon;
+
+namespace DestIstok
+{
+    public class PlugIn : PlugInULoaderDest
+    {
+        //private Dictionary <int, HMark> m_dictMarkDataHost;
+
+        public PlugIn()
+            : base()
+        {
+            _Id = 207;
+
+            registerType(20701, typeof(DestIstokSql));
+            registerType(20702, typeof(DestIstokDSql));
+        }
+
+        public override void OnEvtDataRecievedHost(object obj)
+        {
+            base.OnEvtDataRecievedHost(obj);
+        }
+    }
+}
