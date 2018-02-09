7afdc20bdd82bcb4f9e462ed2e7072d947f25530
Chrjapin_AN <Chrjapin_AN@6f97f952-99cc-4844-926e-59bc75dff8f2>
2018-02-09 08:40:41 +0000

    TestFunc.HandlerUnRegister
    git-svn-id: svn:///U:/Repos/uloader@480 6f97f952-99cc-4844-926e-59bc75dff8f2

---
+++ trunk/TestFunc/HandlerUnRegister.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestFunc
+{
+    class HandlerUnRegister
+    {
+        private class A
+        {
+            public event Action Event;
+
+            public void PerformEvent ()
+            {
+                Event?.Invoke ();
+            }
+
+            public void UnRegister (Type type)
+            {
+                //TODO:
+            }
+        }
+
+        private class B
+        {
+            public void OnEvent ()
+            {
+            }
+        }
+
+        private class C
+        {
+            public void OnEvent ()
+            {
+                throw new Exception ("Задача не выполнена...");
+            }
+        }
+
+        public HandlerUnRegister ()
+        {
+            A a = new A();
+            B b = new B ();
+            C c = new C ();
+
+            a.Event += b.OnEvent;
+            a.Event += c.OnEvent;
+
+            a.UnRegister (c.GetType());
+
+            a.PerformEvent ();
+        }
+    }
+}
